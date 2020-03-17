41ac49b775bf13fccfd82cbf4bc3838646b8be6b
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-17 19:34:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,3 @@
 def nyc_pigeon_organizer(data)
-  new_pigeon_data = {}
-  data.each do |property, hash|
-    hash.each do |attribute, array|
-      array.each do |name|
-        if !new_pigeon_data.has_key?(name)
-          new_pigeon_data[name] = {}
-        end
+  
 end
