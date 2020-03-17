3f7b461e9298409a8630c60cecbf08d7b960a9fb
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-17 19:32:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,4 @@
 def nyc_pigeon_organizer(data)
   new_pigeon_data = {}
-  data.each do |attribute_category, attribute_data_hash|
-    attribute_data_hash.each do |attribute, name_array|
-      name_array.each do |name|
-        new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}  #establish top tier
-      end
-    end
-  end
+  
 end
