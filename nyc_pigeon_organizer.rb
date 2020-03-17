fc793684553efab29fb4a09234f04e2da6b15e1f
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-17 19:58:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_list = {}
+  data.each do |key,values|
+    values.each do |value,names|
+      names.each do |name|
+        new_list[name] ||= {}
+        new_list[name][key] ||= []
+        new_list[name][key] << value.to_s
+      end
+    end
+  end
+  new_list
 end
